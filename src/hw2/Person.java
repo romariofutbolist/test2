cb624f515a189af751802934c8e7df87ce05b156
romariofutbolist <66all@mail.ru>
2023-12-24 16:38:39 +0300

    test2 - hw2 сдача

---
+++ src/hw2/Person.java
@@ -0,0 +1,18 @@
+package hw2;
+
+public class Person {
+    private String fullName;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public void talk() {
+        System.out.println("Person говорит");
+    }
+}
